cef9190d35248021ee868677a67e647127b2b927
saransh <sethia.saransh.ss@gmail.com>
2024-11-06 14:46:42 +0530

    third commit

--- src/Home.js
+++ src/Home.js
@@ -1,54 +1,63 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
-const Home = () =>    {
-    const [name, setName] = useState("");
-    const [input, setInput] = useState({
-        firstName: "",
-        lastName: ""
+const Home = () => {
+  const [name, setName] = useState("");
+  const [input, setInput] = useState({
+    firstName: "",
+    lastName: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setInput((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = () => {
+    if(input.firstName === "" || input.lastName === ""){
+        return;
+    }
+    let newData = { ...input };
+    setName(newData);
+    setInput({
+      firstName: "",
+      lastName: "",
     });
+  };
 
-    const handleChange = (event) => {
-        const {name, value} = event.target;
-                setInput((prev)=>({...prev, [name]:value}))
-    }
+  return (
+    <div>
+      <h1>Full Name Display</h1>
+      <form>
+        <label>First Name: </label>
+        <input
+          type="text"
+          name="firstName"
+          value={input.firstName}
+          onChange={handleChange}
+          required="true"
+        />
+        <br />
+        <label>Last Name: </label>
+        <input
+          type="text"
+          name="lastName"
+          value={input.lastName}
+          onChange={handleChange}
+          required="true"
+        />
+        <br />
+        <button  onClick={() => handleSubmit()}>
+          Submit
+        </button>
+      </form>
+ 
+        <p>
+        Full Name: {name.firstName} {name.lastName}
+      </p>
+      
+ 
+    </div>
+  );
+};
 
-    const handleSubmit = () => {
-
-            let newData = {...input};
-        setName(newData);
-            setInput({
-                firstName:"",
-                lastName:""
-            })
-    }
-    
-    return(
-        <div>
-            <h1>Full Name Display</h1>
-            <form>
-                <label>First Name: </label>
-                <input
-                    type="text"
-                    name="firstName"
-                    value={input.firstName}
-                    onChange={handleChange}
-                    required="true"
-                    />
-                <br />
-                <label>Last Name: </label>
-                <input
-                    type="text"
-                    name="lastName"
-                    value={input.lastName}
-                    onChange={handleChange}
-                    required="true"
-                    />
-                <br />
-                <button type="submit" onClick={()=>handleSubmit()}>Submit</button>
-            </form>
-            <p>Full Name: {name.firstName} {" "} {name.lastName}</p>
-        </div>
-    )
-}
-
-export default Home
+export default Home;
